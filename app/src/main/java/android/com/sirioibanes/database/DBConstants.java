bb6f2a4d2ce755665328574aef2f79eafeec221a
LucasN <lucasnunez9710@gmail.com>
2018-04-13 14:18:29 -0300

    gfkfjfArchivo de la app

--- app/src/main/java/android/com/sirioibanes/database/DBConstants.java
+++
@@ -1,9 +0,0 @@
-package android.com.sirioibanes.database;
-
-public final class DBConstants {
-    public static final String TABLE_CODES = "Codigos";
-    public static final String TABLE_EVENTS = "Eventos";
-    public static final String TABLE_USERS = "Users";
-    public static final String TABLE_ASSIGNMENTS = "Asignaciones";
-    public static final String TABLE_MUSIC = "Musica";
-}
