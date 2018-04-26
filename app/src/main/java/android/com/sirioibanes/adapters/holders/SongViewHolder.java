48d96779cc063c53d0b86dd6322ac417cfaf3817
Federico Bustos Fierro <fedebustosfierro@gmail.com>
2018-04-26 01:18:32 -0300

    label artista funcionando

--- app/src/main/java/android/com/sirioibanes/adapters/holders/SongViewHolder.java
+++ app/src/main/java/android/com/sirioibanes/adapters/holders/SongViewHolder.java
@@ -11,25 +11,35 @@ import android.widget.TextView;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+//ARRIBA DE ESTAN LAS DEPENDENCIAS DE ESTA CLASE
+
 public class SongViewHolder extends RecyclerView.ViewHolder {
 
+    //VARIABLES
     public static final int VOTE_DOWN = 0;
     public static final int VOTE_UP = 1;
+    private final TextView artistLabel;
 
     @IntDef({VOTE_DOWN, VOTE_UP})
     @Retention(RetentionPolicy.SOURCE)
+
+
+    //METODOS
     public @interface VoteType {
     }
 
     public SongViewHolder(View itemView) {
+        //este metodo se ejecuta al inicializar de la celda
         super(itemView);
+        artistLabel = (TextView) itemView.findViewById(R.id.artistLabel);
     }
 
     public void onBind(@NonNull final Song song) {
-        final String artist = song.getArtista() == null ? "Desconocido" : song.getArtista();
-//
-//        ((TextView) itemView.findViewById(R.id.artistView)).setText(artist);
-//        ((TextView) itemView.findViewById(R.id.songView)).setText(song.getTema());
-//        ((TextView) itemView.findViewById(R.id.counterView)).setText(String.valueOf(song.getVotos()));
+        //este metodo se ejecuta cuando queremos impactar la informacion de una cancion
+        String artistText = "Artista desconocido";
+        if (song.artista != null) {
+            artistText = song.artista;
+        }
+        artistLabel.setText(artistText);
     }
 }
