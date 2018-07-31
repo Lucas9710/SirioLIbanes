be53c329e3b0bfb379e29a15d41a2054856048c1
LucasN <lucasnunez9710@gmail.com>
2018-07-31 16:59:58 -0300

    Dideño no invitado

---
+++ app/src/main/java/android/com/sirioibanes/activities/NoinvitadoActivity.java
@@ -0,0 +1,13 @@
+package android.com.sirioibanes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoinvitadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noinvitado);
+    }
+}
