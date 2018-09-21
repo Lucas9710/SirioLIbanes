d06aa2dc24fd35cb7be95b1a28c0b11ddae72c15
LucasN <lucasnunez9710@gmail.com>
2018-09-21 14:46:08 -0300

    pantalla regalos en progreso

---
+++ app/src/main/java/android/com/sirioibanes/activities/GiftActivity.java
@@ -0,0 +1,14 @@
+package android.com.sirioibanes.activities;
+
+import android.com.sirioibanes.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gift);
+    }
+}
